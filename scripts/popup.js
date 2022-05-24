8c7175e73a7856f9130e399041c1f9900d11f391
Junior marques <jrmarqueshd@gmail.com>
2022-05-23 21:47:50 -0300

    build(deps): update manifest to version 3

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,26 +4,18 @@ var jsonViewer = new JSONViewer();
 
 document.querySelector("#json").appendChild(jsonViewer.getContainer());
 
-class Console {
-	log = (data) => {
-		return chrome.extension.getBackgroundPage().console.log(data);
-	};
-}
-
-const _console = new Console();
-
 const List = document.getElementById("list");
 
-function onSelectKey(id) {
+function onSelectKey(itemId) {
 	try {
-		chrome.tabs.query(
-			{ currentWindow: true, active: true },
-			async function ([tabs]) {
-				const [response] = await chrome.tabs.executeScript(tabs.id, {
-					code: `localStorage['${id}']`,
+		chrome.tabs.query({ currentWindow: true, active: true }, async function ([tab]) {
+				const [response] = await chrome.scripting.executeScript({
+					target: {tabId: tab.id},
+					func: (itemId) => localStorage.getItem(itemId),
+					args: [itemId]
 				});
 
-				let _obj = JSON.parse(response);
+				let _obj = JSON.parse(response.result);
 
 				Object.keys(_obj).forEach((name) => {
 					_obj = {
@@ -36,25 +28,29 @@ function onSelectKey(id) {
 			}
 		);
 	} catch (error) {
-		_console.log(error);
+		console.log(error);
 	}
 }
+chrome.tabs.query({ currentWindow: true, active: true }, async function ([tab]) {
+	try{
+		const [response] = await chrome.scripting.executeScript({
+			target: {tabId: tab.id},
+			func: () => Object.keys(localStorage)
+		});
 
-chrome.tabs.query({ currentWindow: true, active: true }, async function (tabs) {
-	const [response] = await chrome.tabs.executeScript(tabs.id, {
-		code: `Object.keys(localStorage)`,
-	});
+		response.result.map((each) =>
+			List.insertAdjacentHTML(
+				"afterbegin",
+				`<li class="button-key" id="${each}" title="${each}">${each}</li>`
+			)
+		);
 
-	response.map((each) =>
-		List.insertAdjacentHTML(
-			"afterbegin",
-			`<li class="button-key" id="${each}" title="${each}">${each}</li>`
-		)
-	);
+		const buttons = document.querySelectorAll(".button-key");
 
-	var buttons = document.querySelectorAll(".button-key");
-
-	buttons.forEach((button) => {
-		button.addEventListener("click", (e) => onSelectKey(e.target.id));
-	});
+		buttons.forEach((button) => {
+			button.addEventListener("click", (e) => onSelectKey(e.target.id));
+		});
+	} catch (error) {
+		console.log(error.message)
+	}
 });
